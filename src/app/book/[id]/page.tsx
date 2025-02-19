79b64a637c1346ae781fba6e26e876076ca374ec
dongmin-dbinc <dmlim@dbinc.co.kr>
2025-02-19 17:29:58 +0900

    init

--- src/app/book/[id]/page.tsx
+++ src/app/book/[id]/page.tsx
@@ -1,8 +1,25 @@
-export default async function Page({
-    params
-}:{
-    params: Promise<{id: string}>;
-}) {
-    const { id } = await params;
-    return <div>book/[id] page: { id }</div>
+import style from "./page.module.css";
+
+export default async function Page({ params }: { params: Promise<{ id: string | string[] }> }) {
+  
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/${params.id}`);
+
+  const { id, title, subTitle, description, author, publisher, coverImgUrl } = mockData;
+
+  return (
+    <div className={style.container}>
+      <div
+        className={style.cover_img_container}
+        style={{ backgroundImage: `url('${coverImgUrl}')` }}
+      >
+        <img src={coverImgUrl} />
+      </div>
+      <div className={style.title}>{title}</div>
+      <div className={style.subTitle}>{subTitle}</div>
+      <div className={style.author}>
+        {author} | {publisher}
+      </div>
+      <div className={style.description}>{description}</div>
+    </div>
+  );
 }
